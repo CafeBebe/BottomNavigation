eed054108190abc7c0b428b66580d30660c0d361
Kondratiev Kirillius <ko.ki.ig1991@gmail.com>
2024-05-30 10:47:56 +0300

    connecting view binding
     creating a bottom panel
    
    creating fragments
    
    writing code in main activity

--- app/src/main/java/com/example/bottomnavigation/MainActivity.kt
+++ app/src/main/java/com/example/bottomnavigation/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.bottomnavigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.bottomnavigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        supportFragmentManager.beginTransaction().replace(R.id.content, Home()).commit()
+
+        binding?.panel?.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+
+                R.id.homeItemBottomNav -> supportFragmentManager.beginTransaction()
+                    .replace(R.id.content, Home()).commit()
+
+                R.id.deliveryItemBottomNav -> supportFragmentManager.beginTransaction()
+                    .replace(R.id.content, Delivery()).commit()
+
+                R.id.contactsItemBottomNav -> supportFragmentManager.beginTransaction()
+                    .replace(R.id.content, Contacts()).commit()
+
+                R.id.catalogItemBottomNav -> supportFragmentManager.beginTransaction()
+                    .replace(R.id.content, Catalog()).commit()
+
+
+
+            }
+
+            return@setOnItemSelectedListener true
+
+
+        }
+
     }
 }
